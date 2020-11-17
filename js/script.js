576aed95a80e4b835f3e3c2fca766a1b2617ab09
KIrill <radhapurnashacti@gmail.ru>
2020-11-17 18:45:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let incr = 10,
+    decr = 10;
+
+   //  ++incr;
+   //  --decr;
+
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(11%2);
+    console.log(2 + 2 * 2 === 8);
+ 
